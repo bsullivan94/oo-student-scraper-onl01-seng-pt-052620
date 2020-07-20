cb92f6a3c967946a44daaef59ff4557bda4c9a1c
brody sullivan <brodysullivan0@gmail.com>
2020-07-20 20:15:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,19 @@ require 'pry'
 
 class Scraper
 
+  def self.scrape_index_page(index_url)
+
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-body-wrapper").each do |student_card|
+      student_card.css("div.student-card a").each do |student|
+      student_name = student.css("h4.student-name").text
+      student_location = student.css("p.student-location").text
+      student_link = "#{student.attr('href')}"
+      students << { name: student_name, location: student_location, profile_url: student_link }
+      end
+    end
+    students
+  end
+  
   
