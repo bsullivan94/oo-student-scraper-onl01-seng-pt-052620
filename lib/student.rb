07a5c8ead458310a92e1ee635cd52d59b9d76a07
brody sullivan <brodysullivan0@gmail.com>
2020-07-20 20:30:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,8 +15,8 @@ class Student
 end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attr, value|
-      self.send("#{attr}=", value)
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
   end
 end
 
